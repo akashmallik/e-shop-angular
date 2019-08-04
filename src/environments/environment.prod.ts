9b77504484e9b80a7412b066a5fa07f94b25bce1
Akash Mallik <hey.knock.me@gamil.com>
2019-08-04 23:10:38 +0600

    e-shop

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCsiM2m1vi22zFD2QVBvQEUY8yawqjc6xQ',
+    authDomain: 'e-shop-app.firebaseapp.com',
+    databaseURL: 'https://e-shop-app.firebaseio.com',
+    projectId: 'e-shop-app',
+    storageBucket: '',
+    messagingSenderId: '107291896564',
+    appId: '1:107291896564:web:b7d728eb6563949b'
+  }
+};
